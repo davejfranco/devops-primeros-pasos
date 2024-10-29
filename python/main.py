9a3ea0a6a51d933e442e44e3f3eeda9231543cd0
Dave Franco <davefranco1987@gmail.com>
2024-10-29 22:06:14 +0100

    python script

---
+++ python/main.py
@@ -0,0 +1,12 @@
+
+
+def sumar():
+    a = int(input("Indique primer valor: "))
+    b = int(input("Indique segundo valor: "))
+    return a + b
+
+def cat(file):
+    f = open(file, 'r')
+    print(f.read())
+
+cat("main.py")
